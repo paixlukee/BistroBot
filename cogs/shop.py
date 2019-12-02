3703ea6a5d68dfaea1c4af3ce914083278547d72
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-12-02 03:49:28 -0500

    Update shop.py

--- cogs/shop.py
+++ cogs/shop.py
@@ -43,7 +43,7 @@ class Shop(commands.Cog):
             ldi = post['items']
             list = sorted(ldi, key=lambda x: x['sold'], reverse=True)
             embed = discord.Embed(colour=0x280071, description=post['description'])
-            print(self.flags[country])#embed.set_author(icon_url=self.flags[country], name=post['name'])
+            embed.set_author(icon_url=str(self.flags[country]), name=post['name'])
             embed.add_field(name="Menu", value=post['items'][0]['name'] + ", " + post['items'][1]['name'] + ", " + post['items'][2]['name'] + f"... To view the full menu, do `r!menu {post['name']}`")
             embed.add_field(name="Most Sold item", value=list[0]['name'])
             embed.add_field(name="Customers", value=post['customers'])
@@ -52,7 +52,7 @@ class Shop(commands.Cog):
             except:
                 pass
             embed.set_footer(text=f"Last Stock: {post['laststock']}")
-            #await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
             
 
     @commands.command(aliases=['Start', 'create'])
