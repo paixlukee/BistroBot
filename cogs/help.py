260028057ec3d3559ccca83169050cb67b764d37
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-12-03 18:00:48 -0500

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -24,11 +24,12 @@ class Help(commands.Cog):
         embed = discord.Embed(description='Welcome to **Restaurant** Here is a list of commands that you are able to use.\n\n'\
         f'`{self.prefix}start` - **Create your own restaurant.**\n'\
         f'`{self.prefix}restaurant [@user]` - **Show restaurant details**\n'\
-        f'`{self.prefix}stock` - **Add more items to your restaurant\'s stock**\n'\
-        f'`{self.prefix}boost` - **Buy a boost or view your current boost**\n'\
-        f'`{self.prefix}daily` - **Receive your daily $200**\n'\
-        f'`{self.prefix}payout` - **Collect your restaurant earnings**\n'\
-        f'`{self.prefix}balance` - **Check your cash balance**\n'
+        f'`{self.prefix}menu <restaurant-name>` - **Add more items to your restaurant\'s stock**\n'\
+        f'`{self.prefix}rate <@user>` - **Buy a boost or view your current boost**\n'\
+        f'`{self.prefix}set` - **Configurate your restaurant settings**\n'\
+        f'`{self.prefix}user [@user]` - **View a user profile**\n'\
+        f'`{self.prefix}balance` - **View your balance**\n'\
+        f'`{self.prefix}donate <@user> <amount>` - **Donate money to someone else**\n'
         )
         embed.set_image(url="https://i.ibb.co/chxrYtn/restaurantbanner.png")
         embed.set_footer(text="Arguments are inside [] and <>. [] is optional and <> is required. Do not include [] or <> in the command.")
