161648155a3b9ab776c0765d68f69a31aae34ab9
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-12-04 22:39:32 -0500

    Update dev.py

--- cogs/dev.py
+++ cogs/dev.py
@@ -22,14 +22,9 @@ class Dev(commands.Cog):
         self.process = psutil.Process(os.getpid())
         self.rs = []
 
-    @commands.command(aliases=['info', 'botinfo', 'status'])
+    @commands.command(aliases=['Stats', 'info', 'botinfo', 'status'])
     @commands.cooldown(1, 2, commands.BucketType.user)
     async def stats(self, ctx):
-        """- Information about myself."""
-        try:
-            await self.add_money(user=ctx.message.author.id, count=1)
-        except:
-            pass
         
         users = len(set(self.bot.get_all_members()))
         channels = []
@@ -44,16 +39,15 @@ class Dev(commands.Cog):
 
         ram = self.process.memory_full_info().rss / 1024**2
 
-        stat = discord.Embed(color=0x37749c, description=f"**Restaurant** `by lukee#0420`\n\n\n" \
+        stat = discord.Embed(colour=0xa82021, description=f"**Restaurant** `by lukee#0420`\n\n\n" \
         f"\> **Python**... `3.6`\n>" \
         f"\> **Ubuntu**... `18.04`\n>" \
         f"\> **RAM Usage**... `{ram:.2f}MB`\n\n"
         f"I am in **{str(len(self.bot.guilds))} servers**!\n"\
-        f"I can see **{channels} channels**!\n"\
+        f"I can view **{channels} channels**!\n"\
         f"I am with **{users} users**!\n"\
         f"I can use **{emojis} emojis**!\n"\
-        f"[DBL](https://discordbots.org/bot/481337766379126784) |"\
-        f" [Invite](https://discordapp.com/api/oauth2/authorize?client_id=648065060559781889&permissions=8192&scope=bot) |"\
+        f"[Invite](https://discordapp.com/api/oauth2/authorize?client_id=648065060559781889&permissions=8192&scope=bot) |"\
         f" [Support](https://discord.gg/BCRtw7c)")
         stat.set_footer(text="Thanks for using Restaurant! | Res-V1")
         await ctx.send(embed=stat)
