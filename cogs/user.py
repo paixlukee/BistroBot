8d62e348abbccf68fabf3f1641a314c0d1aefc6e
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-12-04 21:08:32 -0500

    Update user.py

--- cogs/user.py
+++ cogs/user.py
@@ -48,7 +48,6 @@ class Shop(commands.Cog):
     @commands.command(pass_context=True)
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def donate(self, ctx, user: discord.User=None, count:int=None):
-        """Give your money to another user"""
         posts_user = db.market.find_one({"owner": user.id})
         posts = db.market.find_one({"owner": ctx.author.id})                          
 
@@ -73,6 +72,53 @@ class Shop(commands.Cog):
             await ctx.send(f"{user.mention}, **{ctx.message.author}** has donated **${count}** to you.")
         else:
             await ctx.send("You don't have an account. Create one by doing `r!start`.") 
+                       
+    @commands.command(pass_context=True, aliases=['Daily'])
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def daily(self, ctx):
+        posts = db.market.find_one({"owner": ctx.author.id})                          
+        count = 200   
+        if posts:
+            await self.add_money(user=ctx.author.id, count=count)
+            await ctx.send(f"{ctx.author.mention}, you've received your daily **${count}**.")
+        else:
+            await ctx.send("You don't have an account. Create one by doing `r!start`.") 
+                       
+    @commands.command(aliases=['Work'])
+    @commands.cooldown(1, 600, commands.BucketType.user)
+    async def work(self, ctx):
+        posts = db.utility.find_one({"utility": "res"})
+        user = db.market.find_one({"owner": ctx.author.id})
+        country = str(posts['country'])
+        rm = rnd(posts['resp'])
+        count = 0
+        r1 = rnd(food.food[country])
+        r2 = rnd(food.food[country])
+        r3 = rnd(food.food[country])
+        r4 = rnd(food.food[country])
+        if 'ITEM' in rm and not 'ITEM2' in rm:
+            count = r1['money']
+            msg = str(rm).replace("ITEM", r1).replace("COUNT", count)
+            await self.add_money(user=ctx.author.id, count=count)
+        elif 'ITEM2' in rm and not 'ITEM4' in rm:
+            count = r1['money']+r2['money']+r3['money']
+            msg = str(rm).replace("ITEM", r1).replace("ITEM2", r2).replace("ITEM3", r3).replace("COUNT", count)
+            await self.add_money(user=ctx.author.id, count=count)
+        else:
+            count = r1['money']+r2['money']+r3['money']+r4['money']
+            msg = str(rm).replace("ITEM", r1).replace("ITEM2", r2).replace("ITEM3", r3).replace("ITEM4", r4).replace("COUNT", count)
+            await self.add_money(user=ctx.author.id, count=count)
+        if 'TIP' in rm and not 'TIP2' in rm:
+            tpc = random.randint(2,4)
+            msg = msg.replace("TIP", tpc)
+            await self.add_money(user=ctx.author.id, count=tpc)
+        else:
+            tpc = random.randint(8,10)
+            msg = msg.replace("TIP", tpc)
+            await self.add_money(user=ctx.author.id, count=tpc)
+                       
+        await ctx.send(embed=f"{ctx.author.mention}, {msg}")
+        
 
     async def add_money(self, user:int, count):
         data = db.market.find_one({"owner": user})
