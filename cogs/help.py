6f89b7b9d99f88d94d32fc3686a4ac1fed1877da
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-12-07 06:03:47 -0500

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -28,11 +28,13 @@ class Help(commands.Cog):
         f'`{self.prefix}menu <restaurant-name>` - **View a restaurant menu**\n'\
         f'`{self.prefix}rate <@user>` - **Rate someone\'s restaurant**\n'\
         f'`{self.prefix}set` - **Configurate your restaurant settings**\n'\
+        f'`{self.prefix}buy` - **Buy an item from the restaurant shop**\n'\                  
         f'`{self.prefix}user [@user]` - **View a user profile**\n'\
         f'`{self.prefix}balance` - **View your balance**\n'\
         f'`{self.prefix}donate <@user> <amount>` - **Donate money to someone else**\n'\
         f'`{self.prefix}daily` - **Receive your daily cash**\n'\
-        f'`{self.prefix}work` - **Work at your restaurant and get money**\n'
+        f'`{self.prefix}work` - **Work at your restaurant and get money**\n'\
+        f'`{self.prefix}clean` - **Clean the restaurant and gain XP**\n'\
         )
         embed.set_image(url="https://i.ibb.co/chxrYtn/restaurantbanner.png")
         embed.set_footer(text="Arguments are inside [] and <>. [] is optional and <> is required. Do not include [] or <> in the command.")
