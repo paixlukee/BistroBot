d7534942ca0d5daaf8c8ffec5d6d6633678d1364
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-12-17 18:06:28 -0500

    Create items.py

---
+++ items.py
@@ -0,0 +1,19 @@
+banners = {
+"common": [
+    {"name": "Confetti Explosion", "url": "https://i.ibb.co/SJpwVNL/banner.jpg"},
+    {"name": "Low Poly Sunset", "url": "https://i.ibb.co/9Ztcbss/banner2.png"},
+    {"name": "Daylight Cafe", "url": "https://i.ibb.co/V2G67TL/banner4.jpg"},
+    {"name": "Soft Eatery", "url": "https://i.ibb.co/gvXrdWq/banner6.jpg"},
+    {"name": "Moonlit Table", "url": "https://i.ibb.co/fSWVkzj/banner7.jpg"},
+    {"name": "Food Cycle", "url": "https://i.ibb.co/j4Gmqq6/banner9.jpg"},
+    {"name": "Sushi Heaven", "url": "https://i.ibb.co/HVL7ct3/banner11.jpg"}
+    ],
+"uncommon": [
+    {"name": "Midnight Meal", "url": "https://i.ibb.co/rx0kRNB/banner3.jpg"},
+    {"name": "Candlelight Dinner", "url": "https://i.ibb.co/G5JKDZ3/banner5.jpg"},
+    {"name": "Beautiful Wedding", "url": "https://i.ibb.co/qCVHXr2/banner8.jpg"},
+    {"name": "Oriental Bliss", "url": "https://i.ibb.co/QC8mxdD/banner10.jpg"},
+    {"name": "Condimenti su Condimenti", "url": "https://i.ibb.co/ryVM1pM/banner12.jpg"},
+    {"name": "Burger Stop", "url": "https://i.ibb.co/SJpwVNL/banner.jpg"}
+    ]
+}
