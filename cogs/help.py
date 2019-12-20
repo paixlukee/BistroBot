1a2b15bf27192c5369ddfe448f4daefa07fb1c86
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-12-20 14:54:05 -0500

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -22,7 +22,10 @@ class Help(commands.Cog):
     @commands.group(aliases=['cmds', 'commands', 'Help'])
     async def help(self, ctx, page=None):
         #keeping this hardcoded, because it will only make me do more work :)
-        page = int(page.lower().replace("#", "").replace("page", ""))
+        try:
+            page = int(page.lower().replace("#", "").replace("page", ""))
+        except:
+            pass
         pages = [1,2,3]
         if not page or not page in pages: 
             embed = discord.Embed(colour=0xa82021, description="Welcome! Here is a list of commands that you are able to use.")
@@ -30,7 +33,7 @@ class Help(commands.Cog):
             embed.add_field(name="Page #2 | User", value="Commands that interact with regular users.")
             embed.add_field(name="Page #3 | Inventory", value="Buy, use, and view items in your inventory.")
             #embed.add_field(name="Page #4 | Configuration", value="Configurate guild-only settings,")
-            embed.set_author(icon_url=ctx.avatar_url_as(format='png'), name="Restaurant Help Manual")
+            embed.set_author(icon_url=ctx.me.avatar_url_as(format='png'), name="Restaurant Help Manual")
             embed.set_image(url="https://i.ibb.co/chxrYtn/restaurantbanner.png")
             embed.set_footer(text="To view a page, put the page number right after the command. Example: r!help 1")
             await ctx.send(embed=embed)
@@ -46,7 +49,7 @@ class Help(commands.Cog):
             embed.add_field(name=f"{self.pr}clean", value="Clean your restaurant and receive EXP")
             embed.add_field(name=f"{self.pr}cook", value="Cook an item and receive EXP")
             embed.add_field(name=f"{self.pr}leaderboard", value="View the global restaurant leaderboard")
-            embed.set_author(icon_url=ctx.avatar_url_as(format='png'), name="Restaurant Help Manual | Page 1")
+            embed.set_author(icon_url=ctx.me.avatar_url_as(format='png'), name="Restaurant Help Manual | Page 1")
             embed.set_image(url="https://i.ibb.co/chxrYtn/restaurantbanner.png")
             embed.set_footer(text="Arguments are inside [] and <>. [] is optional and <> is required. Do not include [] or <> in the command.")
             await ctx.send(embed=embed)
@@ -55,7 +58,7 @@ class Help(commands.Cog):
             embed.add_field(name=f"{self.pr}profile [@user]", value="View a user profile")
             embed.add_field(name=f"{self.pr}balance", value="View your balanece")
             embed.add_field(name=f"{self.pr}donate <@user> <amount>", value="Donate money to someone else")
-            embed.set_author(icon_url=ctx.avatar_url_as(format='png'), name="Restaurant Help Manual | Page 2")
+            embed.set_author(icon_url=ctx.me.avatar_url_as(format='png'), name="Restaurant Help Manual | Page 2")
             embed.set_image(url="https://i.ibb.co/chxrYtn/restaurantbanner.png")
             embed.set_footer(text="Arguments are inside [] and <>. [] is optional and <> is required. Do not include [] or <> in the command.")
             await ctx.send(embed=embed)
@@ -64,7 +67,7 @@ class Help(commands.Cog):
             embed.add_field(name=f"{self.pr}inventory", value="View your inventory")
             embed.add_field(name=f"{self.pr}use <item>", value="View your balanece")
             embed.add_field(name=f"{self.pr}buy", value="View shop and buy items")
-            embed.set_author(icon_url=ctx.avatar_url_as(format='png'), name="Restaurant Help Manual | Page 3")
+            embed.set_author(icon_url=ctx.me.avatar_url_as(format='png'), name="Restaurant Help Manual | Page 3")
             embed.set_image(url="https://i.ibb.co/chxrYtn/restaurantbanner.png")
             embed.set_footer(text="Arguments are inside [] and <>. [] is optional and <> is required. Do not include [] or <> in the command.")
             await ctx.send(embed=embed)
