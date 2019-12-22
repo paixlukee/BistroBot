235bc6e07264f4d1353916db645d9612ba20e327
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-12-22 17:36:50 -0500

    Update shop.py

--- cogs/shop.py
+++ cogs/shop.py
@@ -607,36 +607,45 @@ class Shop(commands.Cog):
             if tt < 6:
                 if resp.content.lower().lower() == word.lower():
                     await ctx.send(f"Perfect! You made a delicious {na} in {round(tt)} seconds! You've earned 20 EXP.")
+                    await self.add_exp(user=ctx.author.id, count=20)
                 else:
                     await ctx.send(f"Uh oh! You failed to unscramble the letter. You've earned 2 EXP for making a bad {na}.")
+                    await self.add_exp(user=ctx.author.id, count=2)
             elif tt < 8:
                 if resp.content.lower() == word.lower():
                     await ctx.send(f"Amazing! You made a tasty {na} in {round(tt)} seconds! You've earned 16 EXP.")
+                    await self.add_exp(user=ctx.author.id, count=16)
                 else:
                     await ctx.send(f"Uh oh! You failed to unscramble the letter. You've earned 1 EXP for making a terrible {na}.")
+                    await self.add_exp(user=ctx.author.id, count=1)
             elif tt < 10:
                 if resp.content.lower() == word.lower():
                     await ctx.send(f"Great! You made a delicious {na} in {round(tt)} seconds! You've earned 12 EXP.")
+                    await self.add_exp(user=ctx.author.id, count=12)
                 else:
                     await ctx.send(f"Uh oh! You failed to unscramble the letter. You've earned 0 EXP for making a disgusting {na}.")
             elif tt < 12:
                 if resp.content.lower() == word.lower():
                     await ctx.send(f"Nice! You made a good {na} in {round(tt)} seconds! You've earned 10 EXP.")
+                    await self.add_exp(user=ctx.author.id, count=10)
                 else:
                     await ctx.send(f"Uh oh! You failed to unscramble the letter. You've earned 0 EXP for making a disgusting {na}.")
             elif tt < 14:
                 if resp.content.lower() == word.lower():
                     await ctx.send(f"OK! You made a not-too-bad {na} in {round(tt)} seconds! You've earned 8 EXP.")
+                    await self.add_exp(user=ctx.author.id, count=8)
                 else:
                     await ctx.send(f"Uh oh! You failed to unscramble the letter. You've earned 0 EXP for making a disgusting {na}.")
             elif tt < 18:
                 if resp.content.lower() == word.lower():
                     await ctx.send(f"Eh! You made an okay {na} in {round(tt)} seconds! You've earned 6 EXP.")
+                    await self.add_exp(user=ctx.author.id, count=6)
                 else:
                     await ctx.send(f"Uh oh! You failed to unscramble the letter. You've earned 0 EXP for making a disgusting {na}.")
             else:
                 if resp.content.lower() == word.lower():
                     await ctx.send(f"Uh oh! You made a disgusting {na} in {round(tt)} seconds! You've earned 1 EXP.")
+                    await self.add_exp(user=ctx.author.id, count=1)
                 else:
                     await ctx.send(f"Uh oh! You failed to unscramble the letter. You've earned 0 EXP for making a disgusting {na}.")
         else:
