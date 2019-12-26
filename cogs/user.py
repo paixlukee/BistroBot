0ca06779bc0e5c88b3306798990d4962029185c3
Luke J <36803432+paixlukee@users.noreply.github.com>
2019-12-26 10:39:16 -0500

    Update user.py

--- cogs/user.py
+++ cogs/user.py
@@ -141,8 +141,7 @@ class User(commands.Cog):
             await ctx.send("You didn't include a restaurant! Example: `r!dine @lukee#0420` or `r!dine McDonalds`.")
         if ctx.message.mentions:
             if len(ctx.message.mentions) >= 2:
-                print(ctx.message.mentions[1])
-                res = db.market.find_one({"owner":ctx.message.mentions[1].id})
+                res = db.market.find_one({"owner":ctx.message.mentions[0].id})
             else:
                 res = db.market.find_one({"owner":ctx.message.mentions[0].id})
         elif db.market.find_one({"name": restaurant}):
