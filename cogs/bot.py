fcbe859f52ef7cdecd1cc551f577e972892195d0
Luke J <36803432+paixlukee@users.noreply.github.com>
2020-04-15 21:52:05 -0400

    Update bot.py

--- cogs/bot.py
+++ cogs/bot.py
@@ -169,10 +169,7 @@ class Botdev(commands.Cog):
             else:
                 hours = ""
             await ctx.send(f"<:RedTick:653464977788895252> You are on cooldown! Please wait **{hours} {round(minutes)}m {round(seconds)}s**.")
-        if isinstance(error, commands.CommandInvokeError):
-            error = getattr(error, 'original', error)
-            if str(error).startswith("403"):
-                await ctx.author.send(f"I need the `send_messages` permission to speak in **#{ctx.channel.name}**!")
+
         else:
             print("\x1b[1;31;40m" + f"[{type(error).__name__}]: " + "\x1b[0m" + str(error))
             ig = (asyncio.futures.TimeoutError, commands.CommandNotFound, commands.CommandOnCooldown, discord.Forbidden, commands.NoPrivateMessage, commands.DisabledCommand, commands.CheckFailure, commands.UserInputError)
