fe6d72d5b337becb9ceed250d4b8d7f51fad8c5d
paixlukee <itslukejeffries@gmail.com>
2020-05-25 23:43:13 -0400

    test pay task

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -13,23 +13,33 @@ import asyncio
 import json
 import os
 import config
+from pymongo import MongoClient
+import pymongo
+
+client = MongoClient(config.mongo_client)
+db = client['siri']
 
 class Tasks(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.index = 0
-        self.printer.start()
+        self.pay.start()
 
     def cog_unload(self):
-        self.printer.cancel()
+        self.pay.cancel()
 
-    @tasks.loop(seconds=5.0)
-    async def printer(self):
-        print(self.index)
-        self.index += 1
+    @tasks.loop(seconds=15)
+    async def pay(self):
+    all = db.market.find()
+    for x in all:
+        if 'worker' in x:
+            if x['worker']:
+                wn = x['worker_name']
+                cash = 1#x['worker'][wn][1]['pay']
+                await self.add_money(user=x.id, count=cash)
+                print('\x1b[1;36;40m' + '[UPDATE]: ' + '\x1b[0m' + 'All Restaurants have been paid.')
 
-    @printer.before_loop
-    async def before_printer(self):
+    @pay.before_loop
+    async def before_pay(self):
         await self.bot.wait_until_ready()
 
 def setup(bot):
