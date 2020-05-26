304f6b49afb11365ec8f83d0314e66f5ade3257f
paixlukee <itslukejeffries@gmail.com>
2020-05-25 23:36:25 -0400

    create tasks

---
+++ cogs/tasks.py
@@ -0,0 +1,36 @@
+import discord
+from discord.ext import commands
+import datetime
+import requests
+import random
+import math
+import time
+from discord.ext.commands import errors, converter
+from random import choice, randint
+from random import choice, randint as rnd
+import aiohttp
+import asyncio
+import json
+import os
+import config
+
+class Tasks(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.index = 0
+        self.printer.start()
+
+    def cog_unload(self):
+        self.printer.cancel()
+
+    @tasks.loop(seconds=5.0)
+    async def printer(self):
+        print(self.index)
+        self.index += 1
+
+    @printer.before_loop
+    async def before_printer(self):
+        await self.bot.wait_until_ready()
+
+def setup(bot):
+    bot.add_cog(Tasks(bot))
