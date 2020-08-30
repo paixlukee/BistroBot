af7b508f78fa12cf1e17e7236d8ccc69ac1c2ad4
paixlukee <itslukejeffries@gmail.com>
2020-08-30 01:39:22 -0400

    fix cook

--- cogs/shop.py
+++ cogs/shop.py
@@ -891,10 +891,15 @@ class Shop(commands.Cog):
                             embed.set_footer(text=f"You're cooking {cfooda}.")
                         await msg.edit(embed=embed)
                         time.sleep(1)
-            loop = asyncio.new_event_loop()
-            asyncio.get_event_loop()
-            task = asyncio.create_task(increase())
-            resp = loop.run_until_complete(self.bot.wait_for('message', check=nc, timeout=240))
+            async def text():
+                loop = asyncio.get_event_loop()
+                inc = loop.create_task(increase())
+                wfm = loop.create_task(self.bot.wait_for('message', check=nc, timeout=240))
+                await inc
+                await wfm
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(main())
+            #resp = loop.run_until_complete(self.bot.wait_for('message', check=nc, timeout=240))
             done = True
         else:
             await ctx.send("<:RedTick:653464977788895252> You don't have a restaurant. Create one with `r!start`.")
