0326adfd03ea958864ad2ae36d3d0d110cbd9a32
paixlukee <itslukejeffries@gmail.com>
2020-10-18 02:49:13 -0400

    t

--- cogs/shop.py
+++ cogs/shop.py
@@ -876,7 +876,7 @@ class Shop(commands.Cog):
             time.sleep(1)
             bar_int = 1
             done = False
-
+            resp = self.bot.wait_for('message', check=nc, timeout=30)
             while bar_int <= 6:
                 if done:
                     pass
@@ -893,7 +893,6 @@ class Shop(commands.Cog):
                         embed.set_footer(text=f"You're cooking {cfooda}.")
                     await msg.edit(embed=embed)
                     await asyncio.sleep(1)
-                    resp = self.bot.wait_for('message', check=nc, timeout=30)
                     if resp.content == 'stop':
                         done = True
                         await ctx.send('worked')
