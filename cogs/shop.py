ad319bc7ccf5ca199f26c2dd83d1c3a89c695851
paixlukee <itslukejeffries@gmail.com>
2021-01-09 05:05:47 -0500

    fix lu

--- cogs/shop.py
+++ cogs/shop.py
@@ -1082,8 +1082,9 @@ class Shop(commands.Cog):
         else:
             #await self.take_exp(ctx.author.id, nextLevel)
             db.market_update_one({"owner": ctx.author.id}, {"$set":{"level": nextLevel}})
-            unlocks = "- " + "\n- ".join(self.unlocks[user['level']})
-            nextUnlocks = "- " + "\n- ".join(self.unlocks[user['level']+1])
+            cl = user['level']
+            unlocks = "- " + "\n- ".join(self.unlocks[cl]})
+            nextUnlocks = "- " + "\n- ".join(self.unlocks[cl+1])
             embed = discord.Embed(description=f"Level up! You've unlocked...\n{unlocks}")
             embed.add_field(name="Next Unlocks...", value=nextUnlocks)
             await ctx.send(embed=embed)
