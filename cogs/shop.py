76a4a72fa7e126b1c5551d095a17a113b54f2952
paixlukee <itslukejeffries@gmail.com>
2021-01-17 14:19:08 -0500

    .

--- cogs/shop.py
+++ cogs/shop.py
@@ -1130,13 +1130,14 @@ class Shop(commands.Cog):
             list = sorted(ldi, key=lambda x: x['sold'], reverse=True)
             embed = discord.Embed(description=post['description'])
             level = str(post['level'])
+            owner = post['owner']
             embed.set_author(icon_url=self.flags[country], name=post['name']) #+ f" {self.levelEmoji[level]}")
             embed.add_field(name=":notepad_spiral: Menu", value=post['items'][0]['name'] + ", " + post['items'][1]['name'] + ", " + post['items'][2]['name'] + f"... To view the full menu, do `r!menu {post['name']}`")
             embed.add_field(name=":bar_chart: Experience", value=format(post['exp'], ",d"))
             embed.add_field(name=":moneybag: Average Price", value="$" + str(average))
             embed.add_field(name=":page_with_curl: Rating", value=stars)
             await ctx.send(f"id > {post['owner']}")
-            embed.add_field(name=":name_badge: Owner", value=str(self.bot.get_user(post['owner'])).replace("None", "Unknown") + badge)
+            embed.add_field(name=":name_badge: Owner", value=self.bot.get_user(owner).replace("None", "Unknown") + badge)
             try:
                 if post['banner']:
                     embed.set_image(url=post['banner'])
