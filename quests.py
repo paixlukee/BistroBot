c2b903830e7fb06289209a15bf2bf2a193e2312e
paixlukee <itslukejeffries@gmail.com>
2021-04-22 00:36:07 -0400

    fix quest

--- quests.py
+++ quests.py
@@ -4,9 +4,9 @@ questlist1 = [
     {"name": "dine_fifty", "description": "Dine in a restaurant 50 times", "rewards": 150, "completed": 0, "total": 50},
 ],
 questlist2 = [
-    {"name": "add_item", "description": "Add a new menu item", "rewards": 50, "completed": False, "link": "http://paixlukee.dev/m/JE2BU.mp4"},
-    {"name": "donate_money", "description": "Donate money to a friend", "rewards": 150, "completed": False, "link": "https://paixlukee.dev/m/CNN7V.mp4"},
-    {"name": "levelup", "description": "Level your restaurant up", "rewards": 250, "completed": False, "link": "https://paixlukee.dev/restaurant/levels"}
+    {"name": "add_item", "description": "Add a new menu item", "rewards": 50, "completed": 0, "link": "http://paixlukee.dev/m/JE2BU.mp4"},
+    {"name": "donate_money", "description": "Donate money to a friend", "rewards": 150, "completed": 0, "link": "https://paixlukee.dev/m/CNN7V.mp4"},
+    {"name": "levelup", "description": "Level your restaurant up", "rewards": 250, "completed": 0, "link": "https://paixlukee.dev/restaurant/levels"}
 ],
 questlist3 = [
     {"name": "earn_money", "description": "Earn $1,000", "rewards": 300, "completed": 0, "total": 1000},
