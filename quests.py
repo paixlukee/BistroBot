2002285cfe653d77e62559f9ba7ce29a8637e690
paixlukee <itslukejeffries@gmail.com>
2021-04-22 00:29:45 -0400

    fix quest rewards

---
+++ quests.py
@@ -0,0 +1,14 @@
+questlist1 = [
+    {"name": "work_hundred", "description": "Work 80 times", "rewards": 200, "completed": 0, "total": 80},
+    {"name": "clean_onefifty", "description": "Clean 100 times", "rewards": 180, "completed": 0, "total": 100},
+    {"name": "dine_fifty", "description": "Dine in a restaurant 50 times", "rewards": 150, "completed": 0, "total": 50},
+],
+questlist2 = [
+    {"name": "add_item", "description": "Add a new menu item", "rewards": 50, "completed": False, "link": "http://paixlukee.dev/m/JE2BU.mp4"},
+    {"name": "donate_money", "description": "Donate money to a friend", "rewards": 150, "completed": False, "link": "https://paixlukee.dev/m/CNN7V.mp4"},
+    {"name": "levelup", "description": "Level your restaurant up", "rewards": 250, "completed": False, "link": "https://paixlukee.dev/restaurant/levels"}
+],
+questlist3 = [
+    {"name": "earn_money", "description": "Earn $1,000", "rewards": 300, "completed": 0, "total": 1000},
+    {"name": "earn_exp", "description": "Earn 300 EXP", "rewards": 300, "completed": 0, "total": 300},
+]
