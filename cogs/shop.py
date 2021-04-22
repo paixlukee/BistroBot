9b6c30da352f9e292ca5aa9e9c1138902ba50677
paixlukee <itslukejeffries@gmail.com>
2021-04-22 00:53:38 -0400

    /.

--- cogs/shop.py
+++ cogs/shop.py
@@ -1285,8 +1285,11 @@ class Shop(commands.Cog):
             t1 = random.choice(quests.questlist1)
             t2 = random.choice(quests.questlist2)
             t3 = random.choice(quests.questlist3)
+            t1n = t1['name']
+            t2n = t2['name']
+            t3n = t3['name']
             db.market.update_one({"owner": ctx.author.id}, {"$set": {"task_list": [t1, t2, t3]}})
-            db.market.update_one({"owner": ctx.author.id}, {"$set": {"tasks": [t1['name'], t2['name'], t3['name']]}})
+            db.market.update_one({"owner": ctx.author.id}, {"$set": {"tasks": [t1n, t2n, t3n]}})
         task1 = post['task_list'][0]
         task2 = post['task_list'][1]
         task3 = post['task_list'][2]
